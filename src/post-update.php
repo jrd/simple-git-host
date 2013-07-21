ec8b5dc039c055aad7ef4f76b764995237a62b8f
Cyrille Pontvieux <jrd@enialis.net>
2013-07-21 18:10:32 +0200

    Add some information where you try to display the post-update.php script without parameters.

--- src/post-update.php
+++ src/post-update.php
@@ -10,5 +10,20 @@ if (isset($_POST['payload'])) {
   $repoName = $_GET['name'];
   $repoUrl = $_GET['url'];
   gitrepoinfo('fetch', $repoName, $repoUrl);
+} else {
+  echo <<<'EOF'
+<html><body><pre>
+You must pass some variables to this script:
+  - name, name of the repository to synchronize to.
+  - url, full url of the repository to synchronize from.
+You can pass it through POST or GET method.
+
+For GET method, just supply these two variables.
+
+For POST method, you should supply a json encoded 'payload' variable.
+This variable should contains a array named 'repository' with 'name' and 'url' variable in it.
+This method is the way Github works.
+</pre></body></html>
+EOF;
 }
 ?>
